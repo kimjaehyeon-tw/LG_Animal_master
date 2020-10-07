0aa745225c98148e16edb0d05b8ada0c9b029275
youngwon992 <syw5164@teleworks.co.kr>
2020-10-07 17:16:02 +0900

    [master] basic setting

--- app/src/main/java/com/example/lg_animal/MainActivity.java
+++ app/src/main/java/com/example/lg_animal/MainActivity.java
@@ -2,66 +2,13 @@ package com.example.lg_animal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String dbName = "pet_test";
-    private static final String[] tableName = {"person", "pet_name", "pet_test"};
-    private String[] names = {"Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread", "Honeycomb", "Ice Cream Sandwich", "Jelly Bean", "Kitkat"};
-
-    ArrayList<HashMap<String, String>> personList;
-    ListView list;
-    private static final String TAG_NAME = "name";
-    private static final String TAG_PHONE ="phone";
-
-    SQLiteDatabase sampleDB = null;
-    ListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        phones = new String[]{"Android 1.5", "Android 1.6", "Android 2.0", "Android 2.2", "Android 2.3", "Android  3.0", "Android  4.0", "Android  4.1", "Android  4.4"};
-
-        list = (ListView) findViewById(R.id.listView);
-        personList = new ArrayList<HashMap<String,String>>();
-
-        try {
-
-
-            sampleDB = this.openOrCreateDatabase(dbName, MODE_PRIVATE, null);
-
-            //테이블이 존재하지 않으면 새로 생성합니다.
-            sampleDB.execSQL("CREATE TABLE IF NOT EXISTS " + tableName
-                    + " (name VARCHAR(20), phone VARCHAR(20) );");
-
-            //테이블이 존재하는 경우 기존 데이터를 지우기 위해서 사용합니다.
-            sampleDB.execSQL("DELETE FROM " + tableName  );
-
-            //새로운 데이터를 테이블에 집어넣습니다..
-            for (int i=0; i<names.length; i++ ) {
-                sampleDB.execSQL("INSERT INTO " + tableName
-                        + " (name, phone)  Values ('" + names[i] + "', '" + phones[i]+"');");
-            }
-
-            sampleDB.close();
-
-        } catch (SQLiteException e) {
-            Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
-            Log.e("", e.getMessage());
-        }
-
-        showList();
     }
 }
